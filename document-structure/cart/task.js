c99d2f441482ec650352e45566ac4e79b59bf605
Dmitriy Tkachenko <Tkachenko_D_V@mail.ru>
2023-12-25 16:32:49 +0900

    Task 3 done

---
+++ document-structure/cart/task.js
@@ -0,0 +1,47 @@
+const products = document.querySelectorAll('.product');
+const cart = document.querySelector('.cart__products');
+
+products.forEach(item => {
+	const value = item.querySelector('.product__quantity-value');
+
+	item.querySelector('.product__quantity-control_dec').onclick = () => {
+		if (value.textContent != 1) {
+			value.textContent = value.textContent - 1;
+		}
+	}
+
+	item.querySelector('.product__quantity-control_inc').onclick = () => {
+		value.textContent = +value.textContent + 1;
+	}
+
+	item.querySelector('.product__add').onclick = () => {
+		AddProduct(item);
+		value.textContent = 1;
+	}
+})
+
+function AddProduct(product) {
+	let quantity = product.querySelector('.product__quantity-value').textContent;
+	const productInCart = Array.from(cart.children).find((item) => item.dataset.id == product.dataset.id)
+
+	if (cart.children.length == 0 || !productInCart) {
+		const newProduct = document.createElement('div');
+
+		newProduct.classList.add('cart__product');
+		newProduct.dataset.id = product.dataset.id;
+		cart.insertAdjacentElement("beforeend", newProduct);
+
+		let img = product.querySelector('.product__image').src;
+
+		newProduct.innerHTML += 
+		`
+			<img class="cart__product-image" src="${img}">
+			<div class="cart__product-count">${quantity}</div>
+		`;
+
+	} else {
+		const totalQuantity = productInCart.querySelector('.cart__product-count');
+		
+		totalQuantity.textContent = +totalQuantity.textContent + +quantity;
+	}
+}
